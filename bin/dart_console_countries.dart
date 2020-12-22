beb4a916138cc34b9cff29d8753cdd5e44459425
Andrés García <aardila@colsof.com.co>
2020-12-22 12:35:56 -0500

    Default code project

---
+++ bin/dart_console_countries.dart
@@ -0,0 +1,5 @@
+import 'package:dart_console_countries/dart_console_countries.dart' as dart_console_countries;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_console_countries.calculate()}!');
+}
---
+++ test/dart_console_countries_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_console_countries/dart_console_countries.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
